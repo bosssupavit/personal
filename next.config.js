742fa4c21cbed2d97c584b5460f6b21e3c9481c2
Supavit Panyafang <supavit.panyafang@gmail.com>
2022-03-11 17:22:25 +0700

    add nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: isProd ? 'https://bosssup.dev' : 'http://localhost:3000',
+  }
 }
 
 module.exports = nextConfig
